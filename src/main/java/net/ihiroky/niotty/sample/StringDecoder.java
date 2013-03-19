ddd684e28294ed4aac5d0d40dc4ff8dbdd9f9c47
Hiroki Itoh <ihiroky@gmail.com>
2013-03-20 06:27:11 +0900

    Fix invalid decoder in sample
    StringDecoder should use CoderBuffer#readString(), so StringEncoder
    encodes a string with CoderBuffer#writeString().

--- src/main/java/net/ihiroky/niotty/sample/StringDecoder.java
+++ src/main/java/net/ihiroky/niotty/sample/StringDecoder.java
@@ -2,15 +2,12 @@ package net.ihiroky.niotty.sample;
 
 import net.ihiroky.niotty.LoadStage;
 import net.ihiroky.niotty.LoadStageContext;
-import net.ihiroky.niotty.buffer.CodecBuffer;
 import net.ihiroky.niotty.TransportStateEvent;
+import net.ihiroky.niotty.buffer.CodecBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CodingErrorAction;
 
 /**
  * Created on 13/01/18, 14:12
@@ -23,18 +20,10 @@ public class StringDecoder implements LoadStage<CodecBuffer, String> {
 
     private static final Charset CHARSET = Charset.forName("UTF-8");
 
-    private CharsetDecoder decoder_ = CHARSET.newDecoder()
-            .onMalformedInput(CodingErrorAction.IGNORE)
-            .onUnmappableCharacter(CodingErrorAction.IGNORE);
-
     @Override
     public void load(LoadStageContext<CodecBuffer, String> context, CodecBuffer input) {
-        try {
-            String s = decoder_.decode(input.toByteBuffer()).toString();
-            context.proceed(s);
-        } catch (CharacterCodingException cce) {
-            cce.printStackTrace();
-        }
+        String s = input.readString(CHARSET.newDecoder());
+        context.proceed(s);
     }
 
     @Override
