87940d94957801dc1e4d4ae7891e6c99cdadb603
Hiroki Itoh <ihiroky@gmail.com>
2014-02-11 20:17:23 +0900

    Add check methods

--- src/main/java/net/ihiroky/niotty/util/Arguments.java
+++ src/main/java/net/ihiroky/niotty/util/Arguments.java
@@ -27,6 +27,22 @@ public final class Arguments {
     }
 
     /**
+     * Checks if the value is not null and not empty.
+     * @param value the value to check
+     * @param name the name of the variable
+     * @return the value
+     * @throws java.lang.IllegalArgumentException if the value is null
+     */
+    public static String requireNonNullAndNonEmpty(String value, String name) {
+        if (value == null || value.isEmpty()) {
+            throw (name != null)
+                    ? new IllegalArgumentException("The " + name + " must not be null or empty.")
+                    : new IllegalArgumentException();
+        }
+        return value;
+    }
+
+    /**
      * Checks if the int value is positive.
      * @param value the value to check
      * @param name the name of the variable
@@ -124,5 +140,21 @@ public final class Arguments {
                     : new IllegalArgumentException();
         }
         return value;
+    }
+
+    /**
+     * Checks if the object is the instance of the specified type.
+     *
+     * @param object the object to be checked
+     * @param name the field name of the object
+     * @param type the class object to represent the type
+     * @param <T> the type
+     * @return the object casted with the type
+     */
+    public static <T> T requireInstanceOf(Object object, String name, Class<T> type) {
+        if (!type.isInstance(object)) {
+            throw new IllegalArgumentException("The " + name + " must be the instance of " + type.getName());
+        }
+        return type.cast(object);
     }
 }
